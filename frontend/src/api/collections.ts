9c2be9fd8140fa43f621de3214f1deda2624a435
aqcd <aquajet173@gmail.com>
2021-10-06 14:46:52 +0800

    abstract out url suffix

--- frontend/src/api/collections.ts
+++ frontend/src/api/collections.ts
@@ -2,6 +2,7 @@ import { ApiPromise } from '../types';
 import { CollectionData, CollectionPostData } from '../types/collections';
 
 import BaseAPI from './base';
+import { URL_SUFFIX } from './helpers/url-suffix';
 
 export class CollectionsAPI extends BaseAPI {
     protected getCollectionUrl(): string {
@@ -10,15 +11,15 @@ export class CollectionsAPI extends BaseAPI {
 
     public addCollection(data: CollectionPostData): ApiPromise<{ collection: CollectionData }> {
         console.log('Adding collection. Data name: ' + data.name);
-        return this.post(`${this.getCollectionUrl()}/`, data);
+        return this.post(`${this.getCollectionUrl()}` + URL_SUFFIX, data);
     }
 
     public patchCollection(id: number, data: CollectionPostData): ApiPromise<{ collection: CollectionData }> {
-        return this.put(`${this.getCollectionUrl()}/${id}/`, data);
+        return this.put(`${this.getCollectionUrl()}/${id}` + URL_SUFFIX, data);
     }
 
     public deleteCollection(id: number): ApiPromise<{}> {
-        return this.delete(`${this.getCollectionUrl()}/${id}/`);
+        return this.delete(`${this.getCollectionUrl()}/${id}` + URL_SUFFIX);
     }
 }
 
