dbef00f62b060d2cc2769d5bd60872e5e5f12593
dlau <danielau88@gmail.com>
2021-10-17 16:01:20 +0800

    Fix trailing characters

--- backend/card/jobs.py
+++ backend/card/jobs.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import os
+import string
 
 from dataclasses import asdict, dataclass
 from shutil import copyfile
@@ -53,12 +54,7 @@ class PaddleOCRResult():
 
 def trim_ocr_text(text: str):
     # TODO: fix trimming
-    text = text.strip()
-    if text[0] == "-":
-        text = text[1:]
-    if text[-1] == "-":
-        text = text[:-1]
-    return text.strip()
+    return text.strip("~|'-" + string.whitespace)
 
 
 def import_card_from_image(image_key: str, collection_id: int, name: str = ""):
